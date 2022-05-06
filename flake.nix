410a953efa39f7394a64fbe5a6be148c0c28f69e
Michael Pontus <m.pontus@gmail.com>
2022-05-06 12:01:59 +0300

    base plugin

--- flake.nix
+++ flake.nix
@@ -1,41 +1,35 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nix-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    let
-      out = system:
-        let pkgs = nixpkgs.legacyPackages."${system}";
-        in {
-
-          # devShell = pkgs.mkShell {
-          #     buildInputs = with pkgs; [
-          #         python3Packages.poetry
-          #     ];
-          # };
-          devShell = with pkgs.poetry2nix;
-            mkPoetryApplication {
-              projectDir = ./.;
-              overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
-                pyparsing = super.pyparsing.overridePythonAttrs (old: {
-                  buildInputs = old.buildInputs ++ [ self.flit-core ];
-                });
-              });
-              # editablePackageSources = { my-app = ./src; };
-            };
-
-          defaultPackage = with pkgs.poetry2nix;
-            mkPoetryApplication {
-              projectDir = ./.;
-              preferWheels = true;
-            };
-
-          defaultApp =
-            utils.lib.mkApp { drv = self.defaultPackage."${system}"; };
-
+  outputs = { self, nixpkgs, nix-utils, nix-filter }:
+    nix-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; };
+      let
+        projectFiles = nix-filter.lib {
+          root = ./.;
+          include = (map nix-filter.lib.inDirectory [ "roombot" ])
+            ++ [ "maubot.yaml" "poetry.lock" "pyproject.toml" ];
         };
-    in with utils.lib; eachSystem defaultSystems out;
-
+        pythonWithPackages = pkgs.poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+          preferWheels = true;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pythonWithPackages
+            python3Packages.poetry
+          ];
+        };
+        defaultPackage = writeShellScriptBin "roombot" ''
+          ${pythonWithPackages}/bin/python -m maubot.standalone \
+            --meta ${projectFiles}/maubot.yaml "$@"
+        '';
+        defaultApp =
+          nix-utils.lib.mkApp { drv = self.defaultPackage."${system}"; };
+      });
 }
---
+++ roombot/__init__.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from maubot import MessageEvent, Plugin
+from maubot.handlers import command
+from mautrix.types import RoomAlias, UserID
+
+
+class RoomBot(Plugin):
+    @command.new(help="Create a new room with bot and operator")
+    @command.argument("room_alias", label="Room alias")
+    async def create(self, evt: MessageEvent, room_alias: RoomAlias) -> None:
+        room_alias = self.parse_room_alias(room_alias)
+        try:
+            room_id = await self.client.create_room(room_alias)
+            await self.client.invite_user(room_id, evt.sender)
+        except Exception as err:
+            self.log.error(err)
+            await evt.reply("{}".format(err))
+
+    @command.new(help="Invite user into the current room")
+    @command.argument("user_id", label="User ID")
+    async def invite(self, evt: MessageEvent, user_id: UserID) -> None:
+        user_id = self.parse_user_id(user_id)
+        try:
+            await self.client.invite_user(evt.room_id, user_id)
+        except Exception as err:
+            self.log.error(err)
+            await evt.reply("{}".format(err))
+
+    @command.new(help="Remove user from the current room")
+    @command.argument("user_id", label="User ID", required=True)
+    async def remove(self, evt: MessageEvent, user_id: UserID) -> None:
+        user_id = self.parse_user_id(user_id)
+        try:
+            await self.client.kick_user(evt.room_id, user_id)
+        except Exception as err:
+            self.log.error(err)
+            await evt.reply("{}".format(err))
+
+    def parse_user_id(self, user_id: str) -> UserID:
+        if not user_id.startswith("@"):
+            user_id = "@" + user_id
+        if not ":" in user_id:
+            user_id = user_id + ":" + self.client.domain
+        return UserID(user_id)
+
+    def parse_room_alias(self, room_alias: str) -> UserID:
+        if not room_alias.startswith("#"):
+            room_alias = "#" + room_alias
+        if not ":" in room_alias:
+            room_alias = room_alias + ":" + self.client.domain
+        return RoomAlias(room_alias)
